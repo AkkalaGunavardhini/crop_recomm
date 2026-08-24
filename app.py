7fb47854bc280f7af56156ce360f98d9890c2521
AkkalaGunavardhini <143833716+AkkalaGunavardhini@users.noreply.github.com>
2026-08-24 17:51:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,28 +3,29 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
+from collections import Counter
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-# Load dataset
+
 data = pd.read_csv("Crop_recommendation.csv")
 
-# Split dataset into features and labels
 X = data.drop(columns=['label'])
 y = data['label']
 
-# Split dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Initialize Random Forest Classifier
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 
-# Train the model
 model.fit(X_train, y_train)
+
+registered_users = {}
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/index')
 def index():
@@ -34,14 +35,16 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        # Get data from form
         features = request.form.to_dict()
-        # Convert data to list for prediction
         new_data = [[float(features['N']), float(features['P']), float(features['K']), float(features['temperature']), float(features['humidity']), float(features['ph']), float(features['rainfall'])]]
-        # Predict crop
-        predicted_crop = model.predict(new_data)
-        return render_template('result.html', predicted_crop=predicted_crop[0])
+        crop_probabilities = model.predict_proba(new_data)[0]
+        top_n = 3
+        top_n_indices = crop_probabilities.argsort()[-top_n:][::-1]
+        top_n_crops = [model.classes_[index] for index in top_n_indices]
+        return render_template('result.html', top_n_crops=top_n_crops)
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
